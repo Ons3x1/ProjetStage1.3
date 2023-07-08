754ed9b4fe3f85b2edea97e3f140447f36b89084
Ons3x1 <133598353+Ons3x1@users.noreply.github.com>
2023-07-08 19:03:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const splash= document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 2000);
+})
+
+function changerContenu() {
+    // Modifier ici le contenu de la page selon vos besoins
+    document.getElementById("contenu").innerHTML = "<div class='new'><h1>Qui sommes Nous?</h1><p>Après une longue période d'exportation du phosphate brut, la Tunisie s'est orientée vers la transformation et la valorisation de ce minerai, et ce par l'implantation d'une industrie locale de production d'Acide Phosphorique et d'Engrais Phosphatés.</p><p>La Tunisie est le deuxième pays au monde à valoriser un grand pourcentage de sa production de phosphate naturel (85%). Le GCT traite environ 6,5 millions de tonnes de phosphate naturel tunisien chaque année pour produire l’Acide Phosphorique Marchand (MGA), le Di-Ammonium Phosphaté (DAP) , le Triple Super Phosphate (TSP) et le Phosphate de Calcium (DCP).</p><p>En outre,  le GCT produit le Nitrate d’Ammonium Agricole et le Nitrate d'Ammonium Poreux destinés principalement au marché local.</p><p>Le GCT dispose de 4 sites de production répartis sur 4 régions du sud tunisien :  Gabes,Sfax , Skhira et M'dhilla. </p><p>Le GCT emploie plus que 6500 personnes, sa mission est définie comme suit:</p> <ul><li>Création d'une forte valeur ajoutée à partir du phosphate tunisien</li><li>Production et exportation de l'Acide Phosphorique et des Engrais Solides à travers le monde</li><li>Création d'emplois et amélioration de la vie sociale des citoyens.</li></ul>  </div> ";
+}
+
+function main() {
+    var element = document.getElementById("element");
+element.style.color = "white";
+
+}
+
+function main2() {
+    var element = document.getElementById("element2");
+element.style.color = "white";
+}
+
+function main3() {
+    var element = document.getElementById("element3");
+element.style.color = "white";
+}
+
+function main4() {
+    var element = document.getElementById("element4");
+element.style.color = "white";
+}
+
+function main4() {
+    var element = document.getElementById("element4");
+element.style.color = "white";
+}
+
+function main5() {
+    var element = document.getElementById("element5");
+element.style.color = "white";
+}
+
+function main6() {
+    var element = document.getElementById("element6");
+element.style.color = "white";
+}
+
+function main7() {
+    var element = document.getElementById("element7");
+element.style.color = "white";
+}
+
+function main8() {
+    var element = document.getElementById("element8");
+element.style.color = "white";
+}
+
+
+function changerContenu2() {
+    // Modifier ici le contenu de la page selon vos besoins
+    document.getElementById("extra").innerHTML = "<p><a class='dropdown-item' href='#'>appels d'offre</a></p><p><a class='dropdown-item' href='#' id='lol'>consultations</a></p>";
+}
